b9e9f2c72bb9dddac2e2b4d5075e6df0168519c6
Gustavo C R Lima <gugacamargorochalima@gmail.com>
2025-07-27 18:02:52 -0300

    ADD histogram equalization

--- aula2.py
+++ aula2.py
@@ -24,9 +24,35 @@ plt.xlabel('Pixel Intensity')
 plt.ylabel('Frequency')
 plt.show()
 
+# Plot colour histogram
 color = ('b','g','r')
 for i,col in enumerate(color):
     histr = cv2.calcHist([original],[i],None,[256],[0,256])
+    plt.plot(histr,color = col)
+    plt.xlim([0,256])
+plt.show()
+
+# Apply histogram equalization
+equalized = cv2.equalizeHist(gray)
+show_image(equalized, cmap='gray', title='Equalized Image')
+
+# Compare histograms
+plt.hist(equalized.ravel(), 256, [0, 256])
+plt.title('Equalized Histogram')
+plt.xlabel('Pixel Intensity')
+plt.ylabel('Frequency')
+plt.show()
+
+# Equalize histogram on all 3 channels
+channels = cv2.split(original)
+eq_channels = [cv2.equalizeHist(ch) for ch in channels]
+equalized_color = cv2.merge(eq_channels)
+show_image(cv2.cvtColor(equalized_color, cv2.COLOR_BGR2RGB), title='Equalized Color Image')
+
+# Plot colour histogram equalized
+color = ('b','g','r')
+for i,col in enumerate(color):
+    histr = cv2.calcHist([equalized_color],[i],None,[256],[0,256])
     plt.plot(histr,color = col)
     plt.xlim([0,256])
 plt.show()
