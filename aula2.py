a368ce6b354dbc8b2ff9c1753d3533b9b4c0beee
Gustavo C R Lima <gugacamargorochalima@gmail.com>
2025-07-28 08:39:27 -0300

    ADD filters and lenna picture

--- aula2.py
+++ aula2.py
@@ -11,48 +11,74 @@ def show_image(img, cmap=None, title=''):
     plt.imshow(img, cmap=cmap)
     plt.show()
 
-original = cv2.imread('exemplo.png')
+original = cv2.imread('lenna.png')
 
 # Load image in grayscale
-gray = cv2.imread('exemplo.png', cv2.IMREAD_GRAYSCALE)
+gray = cv2.imread('lenna.png', cv2.IMREAD_GRAYSCALE)
 show_image(gray, cmap='gray', title='Original Grayscale Image')
 
 # Plot histogram
-plt.hist(gray.ravel(), 256, [0, 256])
-plt.title('Histogram')
-plt.xlabel('Pixel Intensity')
-plt.ylabel('Frequency')
-plt.show()
+#plt.hist(gray.ravel(), 256, [0, 256])
+#plt.title('Histogram')
+#plt.xlabel('Pixel Intensity')
+#plt.ylabel('Frequency')
+#plt.show()
 
 # Plot colour histogram
-color = ('b','g','r')
-for i,col in enumerate(color):
-    histr = cv2.calcHist([original],[i],None,[256],[0,256])
-    plt.plot(histr,color = col)
-    plt.xlim([0,256])
-plt.show()
+#color = ('b','g','r')
+#for i,col in enumerate(color):
+#    histr = cv2.calcHist([original],[i],None,[256],[0,256])
+#    plt.plot(histr,color = col)
+#    plt.xlim([0,256])
+#plt.show()
 
 # Apply histogram equalization
-equalized = cv2.equalizeHist(gray)
-show_image(equalized, cmap='gray', title='Equalized Image')
+#equalized = cv2.equalizeHist(gray)
+#show_image(equalized, cmap='gray', title='Equalized Image')
 
 # Compare histograms
-plt.hist(equalized.ravel(), 256, [0, 256])
-plt.title('Equalized Histogram')
-plt.xlabel('Pixel Intensity')
-plt.ylabel('Frequency')
-plt.show()
+#plt.hist(equalized.ravel(), 256, [0, 256])
+#plt.title('Equalized Histogram')
+#plt.xlabel('Pixel Intensity')
+#plt.ylabel('Frequency')
+#plt.show()
 
 # Equalize histogram on all 3 channels
-channels = cv2.split(original)
-eq_channels = [cv2.equalizeHist(ch) for ch in channels]
-equalized_color = cv2.merge(eq_channels)
-show_image(cv2.cvtColor(equalized_color, cv2.COLOR_BGR2RGB), title='Equalized Color Image')
+#channels = cv2.split(original)
+#eq_channels = [cv2.equalizeHist(ch) for ch in channels]
+#equalized_color = cv2.merge(eq_channels)
+#show_image(cv2.cvtColor(equalized_color, cv2.COLOR_BGR2RGB), title='Equalized Color Image')
 
 # Plot colour histogram equalized
-color = ('b','g','r')
-for i,col in enumerate(color):
-    histr = cv2.calcHist([equalized_color],[i],None,[256],[0,256])
-    plt.plot(histr,color = col)
-    plt.xlim([0,256])
-plt.show()
+#color = ('b','g','r')
+#for i,col in enumerate(color):
+#    histr = cv2.calcHist([equalized_color],[i],None,[256],[0,256])
+#    plt.plot(histr,color = col)
+#    plt.xlim([0,256])
+#plt.show()
+
+# Add Gaussian noise to a colored image
+mean = 0
+stddev = 50
+gaussian_noise_color = np.random.normal(mean, stddev, original.shape).astype(np.float32)
+noisy_color_image = cv2.add(original.astype(np.float32), gaussian_noise_color)
+noisy_color_image = np.clip(noisy_color_image, 0, 255).astype(np.uint8)
+show_image(cv2.cvtColor(noisy_color_image, cv2.COLOR_BGR2RGB), title='Noisy Color Image')
+
+cv2.imwrite('noise_lenna.png', noisy_color_image)
+
+# Mean filter
+blur_mean = cv2.blur(noisy_color_image, (10, 10))
+show_image(cv2.cvtColor(blur_mean, cv2.COLOR_BGR2RGB), cmap=None, title='Mean Blur')
+
+# Gaussian filter
+blur_gaussian = cv2.GaussianBlur(noisy_color_image, (13, 13), 0)
+show_image(cv2.cvtColor(blur_gaussian, cv2.COLOR_BGR2RGB), cmap=None, title='Gaussian Blur')
+
+# Median filter
+blur_median = cv2.medianBlur(noisy_color_image, 9)
+show_image(cv2.cvtColor(blur_median, cv2.COLOR_BGR2RGB), cmap=None, title='Median Blur')
+
+# Bilateral filter
+bilateral = cv2.bilateralFilter(noisy_color_image,9,75,75)
+show_image(cv2.cvtColor(bilateral, cv2.COLOR_BGR2RGB), cmap=None, title='Sharpened Image opencv')
