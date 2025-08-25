0a65f3dfd73320735992fd3f11d1c4b5a9665bc3
Gustavo C R Lima <gugacamargorochalima@gmail.com>
2025-08-25 07:45:42 -0300

    ADD create image

--- aula4.py
+++ aula4.py
@@ -13,3 +13,11 @@ def show_image(img, cmap=None, title=''):
         # OpenCV loads BGR, convert to RGB for display
         plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     plt.show()
+
+h, w = 240, 360
+image = np.zeros((h, w), dtype=np.uint8)
+cv2.circle(image, (70, 80), 30, 255, -1)
+cv2.circle(image, (180, 80), 30, 255, -1)
+cv2.circle(image, (290, 80), 30, 255, -1)
+cv2.circle(image, (120, 170), 25, 255, -1)
+cv2.circle(image, (250, 170), 25, 255, -1)
